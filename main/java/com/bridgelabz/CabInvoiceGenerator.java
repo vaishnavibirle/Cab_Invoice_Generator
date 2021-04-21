07cbf64fb92aae1f1b46dd27d03f06e3db7114f1
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-04-21 11:11:58 +0530

    [vaishnavi]:ADDED Welcome message

---
+++ main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("****WELCOME TO CAB INVOICE GENERATOR****");
+    }
+}
---
+++ test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+
+public class CabInvoiceGeneratorTest {
+
+
+}
